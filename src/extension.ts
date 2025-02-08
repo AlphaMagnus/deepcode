703c3b8e5b32b0566563378c9d8fe8cae74fb8f7
AlphaMagnus <varupandya2412@gmail.com>
2025-02-08 11:45:52 +0530

    initial

---
+++ src/extension.ts
@@ -0,0 +1,92 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+import ollama from "ollama";
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "deepseek-chat" is now active!');
+	vscode.window.showInformationMessage("Hello World from deepseek-chat!");
+  
+	const panel = vscode.window.createWebviewPanel(
+	  "deepseek-chat", // Identifies the type of the webview. Used internally
+	  "DeepSeek Chat", // Title of the panel displayed to the user
+	  vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+	  {
+		enableForms: true,
+		enableScripts: true,
+	  }
+	);
+  
+	panel.webview.onDidReceiveMessage(async (message) => {
+	  if (message.type === "user-prompt") {
+		const { text } = message;
+  
+		const stream = await ollama.chat({
+		  model: "deepseek-r1:8b",
+		  messages: [{ role: "user", content: text }],
+		  stream: true,
+		});
+  
+		let fullResponse = "";
+		for await (const chunk of stream) {
+		  const { content } = chunk.message;
+		  fullResponse += content;
+		  panel.webview.postMessage({ type: "agent", text: fullResponse });
+		}
+	  }
+	});
+  
+	panel.webview.html = /*html*/ `
+  <html>
+	<head>
+	  <style>
+		body {
+		  font-family: sans-serif;
+		  padding: 20px;
+		}
+  
+		input {
+		  min-width: 50vw;
+		}
+  
+	  </style>
+	</head>
+	<body>
+	  <h1>DeepCode</h1>
+	  <form>
+		<input name="prompt" placeholder="How Can I Help" />
+		<button type="submit">Generate</button>
+		<div id="response"></div>
+	  </form>
+	</body>
+  
+	<script>
+	  const vscode = acquireVsCodeApi();
+  
+	  document.querySelector("form").addEventListener("submit", (e) => {
+		e.preventDefault();
+		const prompt = document.querySelector("input").value;
+		vscode.postMessage({ type: "user-prompt", text: prompt });
+	  })
+  
+	  window.addEventListener("message", (event) => {
+		const message = event.data
+  
+		if (message.type === "agent") {
+		  document.querySelector("#response").innerHTML = marked.parse(message.text);
+		}
+	  })
+  
+	</script>
+  
+	<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js" async></script>
+  
+  </html>
+	`;
+  }
+  
+  // This method is called when your extension is deactivated
+  export function deactivate() {}
